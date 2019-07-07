17105b54a998fd398b2b93f6368d3b74b1e791ae
Brandi Apetsi <brandiapetsi@gmail.com>
2019-07-07 12:01:22 -0400

    Working get request

---
+++ lambda/lambda-router.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const knex = require('knex');
+
+const knexConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: './data/lambda.db3'
+  },
+  debug: true,
+};
+
+const db = knex(knexConfig);
+
+router.get('/', (req, res) => {
+  db('zoos')
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+module.exports = router;
