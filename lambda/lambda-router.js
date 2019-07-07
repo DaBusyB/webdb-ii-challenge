6803c8081d3b68f9b0b47577646656f940dbc326
Brandi Apetsi <brandiapetsi@gmail.com>
2019-07-07 13:29:46 -0400

    Working post, get by id, and put requests

--- lambda/lambda-router.js
+++ lambda/lambda-router.js
@@ -12,6 +12,17 @@ const knexConfig = {
 
 const db = knex(knexConfig);
 
+router.post('/', (req, res) => {
+    db('zoos')
+    .insert(req.body)
+    .then(newZoo => {
+        res.status(201).json(newZoo)
+    })
+    .catch(error => {
+        res.status(500).json(error)
+    })
+})
+
 router.get('/', (req, res) => {
   db('zoos')
     .then(zoos => {
@@ -21,5 +32,36 @@ router.get('/', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+router.get('/:id', (req, res) => {
+    const zooId = req.params.id
+
+    db('zoos')
+    .where({id: zooId})
+    .first()
+
+    .then(zoo => {
+        res.status(200).json(zoo)
+    })
+    .catch(error => {
+        res.status(500).json(error)
+    })
+})
+
+router.put('/:id', (req, res) => {
+    db('zoos')
+    .where({id: req.params.id})
+    .update(req.body)
+    .then(count => {
+        if(count > 0) {
+            res.sratus(200).json(count)
+        } else {
+            res.status(404).json({message: 'Record not found'})
+        }
+    })
+    .catch(error => {
+        res.status(500).json(error)
+    })
+})
 
 module.exports = router;
