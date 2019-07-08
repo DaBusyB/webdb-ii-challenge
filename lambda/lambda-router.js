cd9452b71fe489ca789be90f2f6155b5c8623ea1
Brandi Apetsi <brandiapetsi@gmail.com>
2019-07-07 20:08:20 -0400

    All endpoints added and working

--- lambda/lambda-router.js
+++ lambda/lambda-router.js
@@ -14,54 +14,71 @@ const db = knex(knexConfig);
 
 router.post('/', (req, res) => {
     db('zoos')
-    .insert(req.body)
-    .then(newZoo => {
-        res.status(201).json(newZoo)
-    })
-    .catch(error => {
-        res.status(500).json(error)
-    })
+        .insert(req.body)
+        .then(newZoo => {
+            res.status(201).json(newZoo)
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
 })
 
 router.get('/', (req, res) => {
-  db('zoos')
-    .then(zoos => {
-      res.status(200).json(zoos);
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+    db('zoos')
+        .then(zoos => {
+        res.status(200).json(zoos);
+        })
+        .catch(error => {
+        res.status(500).json(error);
+        });
 });
 
 router.get('/:id', (req, res) => {
     const zooId = req.params.id
 
     db('zoos')
-    .where({id: zooId})
-    .first()
+        .where({id: zooId})
+        .first()
 
-    .then(zoo => {
-        res.status(200).json(zoo)
-    })
-    .catch(error => {
-        res.status(500).json(error)
-    })
+        .then(zoo => {
+            res.status(200).json(zoo)
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
 })
 
 router.put('/:id', (req, res) => {
     db('zoos')
-    .where({id: req.params.id})
-    .update(req.body)
-    .then(count => {
-        if(count > 0) {
-            res.sratus(200).json(count)
-        } else {
-            res.status(404).json({message: 'Record not found'})
-        }
-    })
-    .catch(error => {
-        res.status(500).json(error)
-    })
+        .where({id: req.params.id})
+        .update(req.body)
+        .then(count => {
+            if(count > 0) {
+                res.sratus(200).json(count)
+            } else {
+                res.status(404).json({message: 'Record not found'})
+            }
+        })  
+        .catch(error => {
+            res.status(500).json(error)
+        })
+})
+
+router.delete('/:id', (req, res) => {
+    db('roles')
+        .where({id: req.params.id})
+        .del()
+        .then(count => {
+            if(count > 0) {
+                res.status(200).json(count)
+            } else {
+                res.status(404)json({message: 'Record not found'})
+            }
+        })
+        .catch(error => {
+            res.status(500)json(error)
+        })
+
 })
 
 module.exports = router;
